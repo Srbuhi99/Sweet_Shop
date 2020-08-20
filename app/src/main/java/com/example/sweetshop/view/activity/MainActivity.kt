5fc9760b83117895d87ffeb7e9a723315c8132a1
Srbuhi Danielyan <srbuhidanielyan2018@gmail.com>
2020-08-20 21:25:26 +0400

    FloatingActionButton, Added navigation component, HomeFragment

--- app/src/main/java/com/example/sweetshop/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/sweetshop/view/activity/MainActivity.kt
@@ -2,12 +2,33 @@ package com.example.sweetshop.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.sweetshop.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val hostt: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment? ?: return
+        val navCont = hostt.navController
+        setUpBottomNav(navCont)
+
+
+    }
+
+    fun setUpBottomNav(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.navigation_view)
+        bottomNav?.setupWithNavController(navController)
+        bottomNav.getOrCreateBadge(R.id.notication_tab).apply {
+            number = 3
+        }
     }
 }
